8af2c7e4badf5c9510959df230a525c2d9f56417
Marcos Suzuki <marcossuzuki@gmail.com>
2021-12-01 09:52:21 +0000

    uso de estilo material

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, request, redirect, url_for, flash, session
 
 DATABASE = "blog.db"
 SECRET_KEY = "pudim"
@@ -25,4 +25,33 @@ def exibir_entradas():
     entradas = []
     for titulo, texto in cur.fetchall():
         entradas.append({'titulo': titulo, 'texto': texto})
-    return str(entradas)
+    return render_template('exibir_entradas.html', entradas = entradas)
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entrada():
+    titulo = request.form['titulo']
+    texto = request.form['texto']
+    sql = "INSERT INTO entradas(titulo, texto) VALUES(?, ?)"
+    g.bd.execute(sql, [titulo, texto])
+    g.bd.commit()
+    flash("Nova entrada registrada com sucesso!!")
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            erro='usuario ou senha errado'
+        else:
+            session['logado']=True
+            flash('Login Efetuado comSucesso!')
+            return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session['logado']=False
+    #session.pop('logado', None)
+    flash('Volte sempre')
+    return redirect(url_for('exibir_entradas'))
